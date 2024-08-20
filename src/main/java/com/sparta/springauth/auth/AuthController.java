eec10461ebc86cc583cb63c32c6dfac837ac3ab9
MOON <moonjs1018@naver.com>
2024-08-20 16:30:03 +0900

    add : 회원가입 구현

--- src/main/java/com/sparta/springauth/auth/AuthController.java
+++ src/main/java/com/sparta/springauth/auth/AuthController.java
@@ -1,5 +1,8 @@
 package com.sparta.springauth.auth;
 
+import com.sparta.springauth.entity.UserRoleEnum;
+import com.sparta.springauth.jwt.JwtUtil;
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -18,9 +21,15 @@ public class AuthController {
 
     public static final String AUTHORIZATION_HEADER = "Authorization";
 
+    private final JwtUtil jwtUtil;
+
+    public AuthController(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
     @GetMapping("/create-cookie")
     public String createCookie(HttpServletResponse res) {
-        addCookie("Roobie Auth", res);
+        addCookie("Moon Auth", res);
 
         return "createCookie";
     }
@@ -34,8 +43,8 @@ public class AuthController {
 
     @GetMapping("/create-session")
     public String createSession(HttpServletRequest req) {
-        HttpSession session = req.getSession(true);
-        session.setAttribute(AUTHORIZATION_HEADER, "Roobie Auth");
+        HttpSession session = req.getSession(true); // true -> 세션을 생성 또는 이미 존재한다면 세션 반환 (그니까 무조건 반환)
+        session.setAttribute(AUTHORIZATION_HEADER, "Moon Auth");
         return "createSession";
     }
 
@@ -46,6 +55,39 @@ public class AuthController {
         String value = (String) session.getAttribute(AUTHORIZATION_HEADER);
         System.out.println("value = " + value);
         return "getSession : " + value;
+    }
+
+    @GetMapping("/create-jwt")
+    public String createJwt(HttpServletResponse res) {
+        // Jwt 생성
+        String token = jwtUtil.createToken("Moon", UserRoleEnum.USER);
+
+        // Jwt 쿠키 저장
+        jwtUtil.addJwtToCookie(token, res);
+
+        return "createJwt : " + token;
+    }
+
+    @GetMapping("/get-jwt") // 이게 어떤 쿠키인줄 알고 정보를 찾지??
+    public String getJwt(@CookieValue(JwtUtil.AUTHORIZATION_HEADER) String tokenValue) {
+        // JWT 토큰 substring
+        String token = jwtUtil.substringToken(tokenValue);
+
+        // 토큰 검증
+        if(!jwtUtil.validateToken(token)){
+            throw new IllegalArgumentException("Token Error");
+        }
+
+        // 토큰에서 사용자 정보 가져오기
+        Claims info = jwtUtil.getUserInfoFromToken(token);
+        // 사용자 username
+        String username = info.getSubject();
+        System.out.println("username = " + username);
+        // 사용자 권한
+        String authority = (String) info.get(JwtUtil.AUTHORIZATION_KEY);
+        System.out.println("authority = " + authority);
+
+        return "getJwt : " + username + ", " + authority;
     }
 
     public static void addCookie(String cookieValue, HttpServletResponse res) {
---
+++ src/main/java/com/sparta/springauth/jwt/JwtUtil.java
@@ -0,0 +1,113 @@
+package com.sparta.springauth.jwt;
+
+import com.sparta.springauth.entity.UserRoleEnum;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    // Header Key 값
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    // 사용자 권한 값의 Key
+    public static final String AUTHORIZATION_KEY = "auth";
+
+    // Token 식별자
+    public static final String BEARER_PREFIX = "Bearer ";
+
+    // Token 만료 시간
+    public static final Long TOKEN_TIME = 60 * 60 * 1000L;
+
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+
+    private Key key;
+
+    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+    // 로그 설정
+    public static final Logger logger = LoggerFactory.getLogger("JWT 관련 로그");
+
+
+    @PostConstruct
+    public void init() {
+        byte[] bytes = Base64.getDecoder().decode(secretKey);
+        key = Keys.hmacShaKeyFor(bytes);
+    }
+
+    // 토큰 생성
+    public String createToken(String username, UserRoleEnum roleEnum) {
+        Date date = new Date();
+
+        return BEARER_PREFIX +
+                Jwts.builder()
+                        .setSubject(username) // 사용자 식별자값
+                        .claim(AUTHORIZATION_KEY, roleEnum) // 사용자 권한 키 밸류
+                        .setExpiration(new Date(date.getTime() + TOKEN_TIME)) // 만료 시간
+                        .setIssuedAt(date) // 발급일
+                        .signWith(key, signatureAlgorithm) // 암호화 알고리즘
+                        .compact();
+    }
+
+    // JWT Cookie 에 저장
+    public void addJwtToCookie(String token, HttpServletResponse res) {
+        try {
+            token = URLEncoder.encode(token, "UTF-8").replaceAll("\\+", "%20");
+
+            Cookie cookie = new Cookie(AUTHORIZATION_HEADER, token);
+            cookie.setPath("/");
+
+            //Response 객체에 Cookie 추가
+            res.addCookie(cookie);
+        } catch (UnsupportedEncodingException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    // JWT 토큰 substring
+    public String substringToken(String tokenValue) {
+        if (StringUtils.hasText(tokenValue) && tokenValue.startsWith(BEARER_PREFIX)) {
+            return tokenValue.substring(7);
+        }
+        logger.error("Not Found Token");
+        throw new NullPointerException("Not Found Token");
+    }
+
+    // 토큰 검증
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (SecurityException | MalformedJwtException | SignatureException e) {
+            logger.error("유효하지 않은 JWT");
+        } catch (ExpiredJwtException e) {
+            logger.error("만료된 토큰");
+        } catch (UnsupportedJwtException e) {
+            logger.error("지원되지 않는 토큰");
+        } catch (IllegalArgumentException e) {
+            logger.error("잘못된 토큰");
+        }
+        return false;
+    }
+
+    // 토큰에서 사용자 정보 가져오기
+    public Claims getUserInfoFromToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+    }
+}
